4420b64ed84416c7bdb03adc84c859109d90845e
Clare Pinder <clare.pinder@thoughtworks.com>
2021-03-11 15:21:34 +0000

    Use useState to save text input to state

--- src/Components/TextInput.js
+++ src/Components/TextInput.js
@@ -1,23 +1,33 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { Button, Container, Form } from 'semantic-ui-react';
 import '../App.css';
 
 function TextInput() {
+
+    const [employeeName, setName] = useState('');
+    const [dob, setDob] = useState('');
+
+    useEffect(() => {
+        console.log(employeeName, dob);
+    })
+    
+
     return ( 
-    <Container fluid className="container">
-    <Form className="form">
-      <Form.Field>
-        <label>Employee's name</label>
-        <input placeholder='Enter their name' />
-      </Form.Field>
-      <Form.Field>
-        <label>Employee's date of birth</label>
-        <input placeholder='Enter their date of birth' />
-      </Form.Field>
-      
-      <Button color="blue" type='submit'>Submit</Button>
-    </Form>
-  </Container>)
+        <Container fluid className="container">
+            <Form className="form">
+            <Form.Field>
+                <label>Employee's name</label>
+                <input placeholder='Enter their name' name = "name" value={employeeName} onChange={e => setName(e.target.value)}/>
+            </Form.Field>
+            <Form.Field>
+                <label>Employee's date of birth</label>
+                <input placeholder='Enter their date of birth' name = "dob" value={dob} onChange={e => setDob(e.target.value)} />
+            </Form.Field>
+            
+            <Button color="blue" type='submit'>Submit</Button>
+            </Form>
+        </Container>)
 }
+console.log()
 
 export { TextInput }
